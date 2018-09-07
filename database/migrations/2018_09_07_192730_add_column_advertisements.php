bc79aed09056b9279c2a4c377213f26aca2025da
Anton <a.silischev@yandex.ru>
2018-09-07 21:44:04 +0300

    upd migrations

--- database/migrations/2018_09_07_192730_add_column_advertisements.php
+++ database/migrations/2018_09_07_192730_add_column_advertisements.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsUsers extends Migration
+class AddColumnAdvertisements extends Migration
 {
     public function up()
     {
-        Schema::table('users', function ($table) {
-            $table->string('address', 100)->nullable();
-            $table->string('phone', 100)->nullable();
+        Schema::table('advertisements', function ($table) {
+            $table->string('address', 100);
+            $table->string('phone', 100);
         });
     }
 
     public function down()
     {
-        Schema::table('users', function ($table) {
+        Schema::table('advertisements', function ($table) {
             $table->dropColumn('address');
             $table->dropColumn('phone');
         });
