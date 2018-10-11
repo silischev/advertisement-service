c5348e54e11384fbbb71242011b555d5ac9a16b9
Anton <a.silischev@yandex.ru>
2018-10-11 21:39:28 +0300

    fix route name

--- resources/views/attributes/index.blade.php
+++ resources/views/attributes/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="list-group">
         @foreach ($types as $type => $name)
-            <a href="{{ route('attributes.type', ['type' => $type]) }}" class="list-group-item">{{ $name }}</a>
+            <a href="{{ route('attributes.list_by_type', ['type' => $type]) }}" class="list-group-item">{{ $name }}</a>
         @endforeach
     </div>
 </div>
